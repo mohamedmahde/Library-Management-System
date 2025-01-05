8e7cecc71038913a73374f412696429de36f87ac
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-01-05 21:38:42 +0200

    Integrating Html Admin Template in Library Project

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>admin</title>
-</head>
-<body>
-    Admin Page
-    
-    <x-app-layout>
-    
-    </x-app-layout>
-    
-</body>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+   @include('admin.header')
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+     @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+    @include('admin.body')
+        @include('admin.footer')
+  </body>
 </html>
